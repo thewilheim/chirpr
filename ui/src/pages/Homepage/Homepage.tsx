fdcdf2365a0f680e07b06fb47f887175779402bc
William Abraham <william.abraham@service.nsw.gov.au>
2025-03-24 10:28:19 +1100

    added navigation to profile from homepage

--- ui/src/pages/Homepage/Homepage.tsx
+++ ui/src/pages/Homepage/Homepage.tsx
@@ -3,14 +3,12 @@ import { FaBell, FaEnvelope, FaRegNewspaper } from "react-icons/fa";
 import { formatViews } from "../../utils";
 import { MdSettings } from "react-icons/md";
 import { useSelector } from "react-redux";
-import {
-  useGetSuggestedFollowersQuery,
-} from "../../slices/userApiSlice";
+import { useGetSuggestedFollowersQuery } from "../../slices/userApiSlice";
 import { IUser } from "../../config/applicatonConfig";
 import { FaCircleUser } from "react-icons/fa6";
 import FollowButton from "../../components/FollowButton";
 import "../../index.css";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import ProfilePicture from "../../components/ProfilePicture";
 import { RootState } from "../../store";
 import Loader from "../../components/Loader";
@@ -24,51 +22,51 @@ function Homepage() {
 
   if (!userInfo) return <Loader />;
   return (
-      <main className="flex text-chirpr-200 h-full">
-        <section className="flex flex-col md:flex-row md:mx-auto w-full md:w-auto">
-          <section className="sticky top-0 w-fill z-50 bg-chirpr-800 p-4 md:hidden">
-            <div className="w-full flex flex-row items-center gap-2 sticky top-0 px-3 ">
-              <IoMdMenu
-                size={34}
-                onClick={() => setToggleMobileDrawer(!toggleMobileDrawer)}
-              />
-              <GiNestBirds size={42} className="flex-1 items-center" />
-              <div className="w-12"></div>
-            </div>
-          </section>
-          <section
-            className={`${
-              toggleMobileDrawer ? "block" : "hidden"
-            } md:block  md:fixed md:left-0 md:p-4 md:h-full md:border-r  md:border-r-white/20 2xl:block 2xl:static 2xl:border-0 2xl:w-[300px] `}
-          >
-            <UserProfileSnippit />
-            <hr className="opacity-50 border-none h-[2px] bg-chirpr-200/40 rounded md:hidden 2xl:block" />
-            <Navigation />
-            <hr className="opacity-50 border-none h-[2px] bg-chirpr-200/40 rounded md:hidden 2xl:block" />
-            <Suggestions />
-          </section>
-          <section
-            className={`flex-row md:border-l md:border-r md:border-white/20 md:mx-16 md:w-[800px] h-full ${
-              toggleMobileDrawer ? "hidden" : "flex"
-            }`}
-          >
-            <article className="flex flex-col border-chirpr-200/20 w-full min-h-screen">
-              <Outlet />
-            </article>
-          </section>
-          <section
-            className={`hidden 2xl:block 2xl:static 2xl:border-0 2xl:w-[300px]`}
-          >
-            <Search />
-            </section>
+    <main className="flex text-chirpr-200 h-full">
+      <section className="flex flex-col md:flex-row md:mx-auto w-full md:w-auto">
+        <section className="sticky top-0 w-fill z-50 bg-chirpr-800 p-4 md:hidden">
+          <div className="w-full flex flex-row items-center gap-2 sticky top-0 px-3 ">
+            <IoMdMenu
+              size={34}
+              onClick={() => setToggleMobileDrawer(!toggleMobileDrawer)}
+            />
+            <GiNestBirds size={42} className="flex-1 items-center" />
+            <div className="w-12"></div>
+          </div>
         </section>
-      </main>
+        <section
+          className={`${
+            toggleMobileDrawer ? "block" : "hidden"
+          } md:block  md:fixed md:left-0 md:p-4 md:h-full md:border-r  md:border-r-white/20 2xl:block 2xl:static 2xl:border-0 2xl:w-[300px] `}
+        >
+          <UserProfileSnippit />
+          <hr className="opacity-50 border-none h-[2px] bg-chirpr-200/40 rounded md:hidden 2xl:block" />
+          <Navigation />
+          <hr className="opacity-50 border-none h-[2px] bg-chirpr-200/40 rounded md:hidden 2xl:block" />
+          <Suggestions />
+        </section>
+        <section
+          className={`flex-row md:border-l md:border-r md:border-white/20 md:mx-16 md:w-[800px] h-full ${
+            toggleMobileDrawer ? "hidden" : "flex"
+          }`}
+        >
+          <article className="flex flex-col border-chirpr-200/20 w-full min-h-screen">
+            <Outlet />
+          </article>
+        </section>
+        <section
+          className={`hidden 2xl:block 2xl:static 2xl:border-0 2xl:w-[300px]`}
+        >
+          <Search />
+        </section>
+      </section>
+    </main>
   );
 }
 
 const UserProfileSnippit = () => {
   const { userInfo: user } = useSelector((state: RootState) => state.auth);
-  console.log(user)
+  console.log(user);
 
   return (
     <div className="flex flex-col items-center text-center p-6 md:hidden 2xl:flex">
@@ -76,20 +74,30 @@ const UserProfileSnippit = () => {
         <GiNestBirds size={46} className="mr-2" />
         Chirpr
       </h1>
-      <ProfilePicture
-        profile_picture_url={user.profile_picture_url}
-        editable={true}
-        width={"w-[96px]"}
-        height={"h-[96px]"}
-      />
+      <Link
+        to={`/profile/${user.id}`}
+        className="flex flex-col justify-center items-center"
+      >
+        <ProfilePicture
+          profile_picture_url={user.profile_picture_url}
+          editable={true}
+          width={"w-[96px]"}
+          height={"h-[96px]"}
+        />
+      </Link>
+      <Link to={`/profile/${user.id}`} className="hover:underline">
       <h2 className="text-xl tracking-wider">
         {user.first_name} {user.last_name}
       </h2>
+      </Link>
       <p className="text-sm opacity-60">@{user.username}</p>
       <p className="mt-2 opacity-60">{user.bio}</p>
       <div className="flex flex-row w-full justify-between mt-6 text-sm text-chirpr-200/45 tracking-wide">
         <p>
-          <strong className="text-chirpr-200">{formatViews(user.numberOfChirps)}</strong> <br />
+          <strong className="text-chirpr-200">
+            {formatViews(user.numberOfChirps)}
+          </strong>{" "}
+          <br />
           Chirps
         </p>
         <p>
